d33431cff81fd86fed2fa806cacdb6444491cc70
Fukuoka68 <seikun68@icloud.com>
2026-04-22 12:21:14 +0900

    更新

---
+++ Assets/FukuokaBranch/enemy_CheckPosition.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/*<Scriptの内容=enemyが画面外に出たかどうかを判定する>
+ * なおenemyは画面買いから出現する為座標範囲は任意とする
+ * 
+ *<Scriptの処理内容>
+ *
+ *
+*/
+
+
+
+public class enemy_CheckPosition : MonoBehaviour
+{
+    //座標範囲
+    [SerializeField] float MAX_x;
+    [SerializeField] float MAX_y;
+    [SerializeField] float MIN_x;
+    [SerializeField] float MIN_y;
+
+
+}
