a1a20a0df901254d1ba577b965911846253fea9a
HiIRo <tsukumo2599@gmail.com>
2026-05-24 19:48:40 +0900

    enemy_genghis（ジンギス）、enemy_zago（ザーゴ）、enemy_tridera（トリデラ）、enemy_ramNeedle（ラムニードル）、enemy_pomoon（ポムーン）追加

---
+++ Assets/Scripts/BETA/Enemy/IEnemyComponent.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// EnemyBaseBeta にアタッチされる全コンポーネントの基底インターフェース
+/// EnemyBaseBeta が初期化・イベント登録のタイミングを統一するために使用する
+/// </summary>
+public interface IEnemyComponent
+{
+	/// <summary>
+	/// EnemyBaseBeta の Start 後に呼ばれる初期化
+	/// GetComponent などは Awake より後であることが保証される
+	/// </summary>
+	void OnEnemyInit(EnemyBaseBeta core);
+}
---
+++ Assets/Scripts/BETA/Enemy/OnHitAddTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 被弾時に制限時間を加算するコンポーネント
+/// enemy_pomoon で使用（被弾のたびに +10）
+/// </summary>
+public class OnHitAddTime : MonoBehaviour, IEnemyComponent
+{
+	[Header("加算設定")]
+	[SerializeField] private int addSeconds = 10; // 被弾時に加算する秒数
+
+	public void OnEnemyInit(EnemyBaseBeta core)
+	{
+		core.OnDamaged += _ => core.AddTime(addSeconds);    // 被弾時に制限時間を加算
+    }
+}
